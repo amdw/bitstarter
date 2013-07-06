d957e2371c5b6a54bdf100ada8cb48a7ca2cfd81
Andrew Medworth <github@medworth.org.uk>
2013-07-06 05:13:50 +0000

    Refactor to allow URL source

--- grader.js
+++ grader.js
@@ -24,33 +24,30 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1);
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1);
     }
     return instr;
-};
-
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerio.load(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +58,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var checkResults = function(data, checksfile) {
+    var checkJson = checkHtml(data, checksfile);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+if(require.main == module) {
+    program
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to test')
+	.parse(process.argv);
+
+    if (program.url) {
+	restler.get(program.url).on('complete', function(data, response) {
+	    // Surely there must be a better way!?
+	    if (data instanceof Error) {
+		console.log("Could not load URL " + program.url + " - " + data.message);
+		process.exit(1);
+	    } else if (response.statusCode != 200) {
+		console.log("Could not load URL " + program.url + " - " + response.statusCode + " " + data);
+		process.exit(1);
+	    } else {
+		checkResults(data, program.checks);
+	    }
+	});
+    } else {
+	checkResults(fs.readFileSync(program.file), program.checks);
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
